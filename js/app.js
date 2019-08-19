10cc5cbc52dd94d36258d7a6caa4781ef8520a25
shauvia <e.szalwinska@gmail.com>
2019-08-18 17:03:22 -0700

    created a function that displays one picture

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function Picture(name, fileName){
   this.name = name;
-  this.fileName = '/img/' + fileName;
+  this.filePath = '/img/' + fileName;
   this.numOfDisplay = 0;
   this.numOfClicks = 0;
   this.pictureID = '';
@@ -18,5 +18,19 @@ function Picture(name, fileName){
 Picture.allPictures = [];
 
 var newPicture = new Picture('R2D2 Bag', 'bag.jpg');
+new Picture('Banana Slicer', 'banana.jpg');
+new Picture('Multipurpose Stand', 'bathroom.jpg');
+new Picture('Wet Rubber Boots', 'boots.jpg');
+
 console.log(newPicture);
+console.log(Picture.allPictures);
+
+// create a function that displays one picture on the page
+function displayOnePicture(){
+  var firstImage = document.getElementById('picture-one');
+  firstImage.src = Picture.allPictures[0].filePath;
+}
+
+displayOnePicture();
+
 

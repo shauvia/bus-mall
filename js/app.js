3eb493c294a090c87fd02104b01742ad5ca14ec8
shauvia <e.szalwinska@gmail.com>
2019-08-18 21:04:45 -0700

    check motes with ideas on how to count clicks on the picture

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ var title2 = document.getElementById('caption_two');
 var title3 = document.getElementById('caption_three');
 
 
-//create a function that generated a rendom number{
+//create a function that generates a random number{
 function getRandomIntInclusive(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -57,20 +57,23 @@ function displaysRandomPicture(imgEl, titleEl){
 } 
 
 //function that ramdomly displays3 pictures;
-function displays3RandomPictures(){
+function displays3RandomPictures(event){
   displaysRandomPicture(img1, title1);
   displaysRandomPicture(img2, title2);
   displaysRandomPicture(img3, title3);
+  console.log(event.target);
 }
 
-displays3RandomPictures();
+// displays3RandomPictures();// displays pictures at the beginning and then event listener listens for click;
 // displayOnePicture(img1, title1, 0);
 // displayOnePicture(img2, title2, 1);
 // displayOnePicture(img3, title3, 3);
 
 //add event listeners that listens fo click on every picture and changes pistures to next three;
 
-img1.addEventListener('click', displays3RandomPictures);
+
+
+img1.addEventListener('click', displays3RandomPictures);// stworzyć  funkcję, która bierze event jako parameter i bedzie wywołana w event listenerze, która liczy ogólną ilość klików i wywywołuje f-cję displays3RandomPictures; wezmie też z event.target.src, aby wiedzieć, które zdjęcie zostało kliknięte; wtedy bede wiedzieć, które zdjęcie zstało kliknięte i bede mogła policzyć ilość klików na zdjęciu i poszukać src w liście(filepath to src) (spojrzeć na console.loga event.target);
 img2.addEventListener('click', displays3RandomPictures);
 img3.addEventListener('click', displays3RandomPictures);
 
